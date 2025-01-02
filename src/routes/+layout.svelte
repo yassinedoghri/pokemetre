b3b779a96f683fe11da3c59818435a147721f32e
Yassine Doghri <yassine@doghri.fr>
2025-01-02 11:57:42 +0000

    build: bootstrap project with sveltekit, deno, tailwindcss, devcontainer and quality tools

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "../app.css";
+  let { children } = $props();
+</script>
+
+<main class="grid min-h-screen place-items-center items-center justify-center">
+  {@render children()}
+</main>
